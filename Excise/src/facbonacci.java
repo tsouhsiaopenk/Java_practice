1f49da5e6c6345a6ec39fce2a789b70f4543ace0
xiaopeng zhou <519888781@qq.com>
2019-12-30 21:25:45 +0800

    找出第n个fabonacci数

---
+++ Excise/src/facbonacci.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class facbonacci {
+    public static int fac(int n){
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        return fac(n-1)+fac(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println(fac(num));
+    }
+}
