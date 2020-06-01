101bde6fb39d4679c661ade2246a2907d135f3bf
xiaopeng zhou <519888781@qq.com>
2020-06-01 20:36:14 +0800

    Math.round()的用法

---
+++ June/src/Java6_1/test.java
@@ -0,0 +1,13 @@
+package Java6_1;
+
+import org.junit.Test;
+
+public class test {
+
+    @Test
+    public void t(){
+        System.out.println(Math.round(11.5));
+        System.out.println(Math.round(-11.5));
+    }
+
+}
