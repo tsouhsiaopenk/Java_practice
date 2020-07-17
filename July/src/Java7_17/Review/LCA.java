5713a3ba52d954dbb3778ace88cf03b1cffbe10d
xiaopeng zhou <519888781@qq.com>
2020-07-17 15:47:13 +0800

    最近公共祖先

---
+++ July/src/Java7_17/Review/LCA.java
@@ -0,0 +1,14 @@
+package Java7_17.Review;
+
+public class LCA {
+    public int getLCA(int a,int b){
+        while (a != b){
+            if (a > b){
+                a /= 2;
+            }else {
+                b /= 2;
+            }
+        }
+        return a;
+    }
+}
